67d06ff729afe290944b787c49d1a7d75b2bf80e
RiesBezemer <ries.bezemer@gmail.com>
2018-10-31 12:27:49 +0100

    V 0.1.21

--- CentraleProject/Test.py
+++ CentraleProject/Test.py
@@ -1,23 +1,26 @@
 import serial                    # import pySerial module
+import time                      # import time module
 # Setup voor Serial Connectie
 com = 'COM3'
 print(com+" wordt opgestart...")
-ComPort = serial.Serial(com)  # open the COM Port
+ComPort = serial.Serial(com)     # open the COM Port
+print(ComPort.name)
 ComPort.baudrate = 9600          # set Baud rate to 9600
 ComPort.bytesize = 8             # Number of data bits = 8
 ComPort.parity   = 'N'           # No parity
 ComPort.stopbits = 1             # Number of Stop bits = 1
 
 # Schrijft data naar de poort
-input = bytearray(b'A')
-No = ComPort.write(input)
+print(ComPort.is_open)
+input = bytearray(b'Test')
+ComPort.write(input)
 print("Data has been send...")
 
 # Leest de data van de poort
 print("Waiting for returning data...")
-output = ComPort.readline()        # Wait and read data
+output = ComPort.readline()       # Wait and read data
 print(output)
 
 # Sluit de seriele connectie af
 print("Port "+com+" wordt afgesloten!")
-ComPort.close()                  # Close the COM Port
+ComPort.close()                   # Close the C/OM Port
